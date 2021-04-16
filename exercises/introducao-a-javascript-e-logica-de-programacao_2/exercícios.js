32e02c58bdded766fa8af46752915479d01a792d
Gabriel <gabriel.omercador@hotmail.com>
2021-04-16 19:34:09 -0300

    .

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercícios.js
@@ -0,0 +1,26 @@
+
+
+// Exercício 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i += 1){
+    console.log(numbers[i]);
+}
+
+// -------------
+
+// Exercício 2
+
+
+let somaNumeros = 0;
+
+for (let index = 0; index < numbers.length; index ++){
+   somaNumeros += numbers[index];   
+}
+console.log(somaNumeros);
+
+// --------------
+
+// Exercicio 3
+
+
